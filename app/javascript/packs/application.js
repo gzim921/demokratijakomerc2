3635bca4367db14a135391f86be941962f3eb240
gzim921 <gzimiseni7@gmail.com>
2021-03-07 20:31:15 +0100

    added javascripot for mobile phone

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,49 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+jQuery(function ($) {
+  $(".fa-bars").click(function () {
+    $(".me").addClass("open");
+  });
+  $(".fa-times").click(function () {
+    $(".me").removeClass("open");
+  });
+
+  // $('.nav-link').click(function () {
+  //     location.reload();
+  // });
+
+  // $("#btnFadeOut").click(function () {
+  //     $('body').addClass('animated fadeOut')
+  // });
+
+  // $("#btnHinge").click(function () {
+  //     $('body').addClass('animated hinge')
+  // });
+
+});
+
+jQuery(function ($) {
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function () {
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+    $animatables = $('.animatable');
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    // Check all animatables and animate them if necessary
+    $animatables.each(function (i) {
+      var $animatable = $(this);
+      if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+      }
+    });
+  };
+
+  // Hook doAnimations on scroll, and trigger a scroll
+  $(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+});
